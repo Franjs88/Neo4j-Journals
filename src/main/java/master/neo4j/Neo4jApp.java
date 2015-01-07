80d6ba76e9594bc4e685cc4565f68d6482748949
Francisco Javier Sánchez Carmona <fran@cloudroom.co>
2015-01-07 19:06:38 +0100

    Initial commit!

---
+++ src/main/java/master/neo4j/Neo4jApp.java
@@ -0,0 +1,61 @@
+package master.neo4j;
+
+/**
+ *
+ * @author fran
+ */
+public class Neo4jApp {
+
+    public Neo4jApp(String filePath) {
+        
+    }
+    
+    /**
+     * Receives directory path in which csv files are located, and populates the 
+     * database with them.
+     * @param path 
+     */
+    public void populate(String path) {
+        
+    }
+    
+    /**
+     *
+     * @param paperName
+     */
+    public void runQ1(String paperName) {
+        
+    }
+    
+    /**
+     *
+     * @param conferenceName
+     */
+    public void runQ2(String conferenceName) {
+        
+    }
+    
+    /**
+     *
+     * @param autorName
+     */
+    public void runQ3(String autorName) {
+        
+    }
+    
+    /**
+     *
+     * @param journalName
+     */
+    public void runQ4(String journalName) {
+        
+    }
+    
+    /**
+     * 
+     * @param args 
+     */
+    public static void main(String[] args) {
+        
+    }
+}
